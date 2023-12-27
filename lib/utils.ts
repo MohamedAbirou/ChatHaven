393dee51ca474edcb01da4d17a87e4b5821c72f5
Mohamed Abirou <109366637+MohamedAbirou@users.noreply.github.com>
2023-12-27 14:33:48 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,46 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function constructMetadata({
+  title = "ChatHaven",
+  description = "",
+  image = "/logo.jpg",
+  icons = "/logo.jpg",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@MohamedAbirou",
+    },
+    icons,
+    metadataBase: new URL("http://localhost:3000/"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
